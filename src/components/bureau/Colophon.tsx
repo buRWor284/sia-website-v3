84505702e0963e6756e1605b8813a2a61a2c20b2
Syed Irfan Ajmal <syedirfanajmal@gmail.com>
2026-06-04 12:21:20 +0500

    Add Spotify link to footer social links

--- src/components/bureau/Colophon.tsx
+++ src/components/bureau/Colophon.tsx
@@ -42,6 +42,7 @@ const COLS: ReadonlyArray<FooterCol> = [
       { label: "LinkedIn ↗",       href: "https://www.linkedin.com/in/syedirfanajmal/",                     external: true },
       { label: "YouTube ↗",        href: "https://youtube.com/@syedirfanajmal/",                             external: true },
       { label: "Apple Podcasts ↗", href: "https://podcasts.apple.com/us/podcast/syed-irfan-ajmal/id1347540466", external: true },
+      { label: "Spotify ↗",        href: "https://creators.spotify.com/pod/profile/syedirfanajmal/episodes/",   external: true },
     ],
   },
 ];
